6fcd7fdb5890a112139313dc377b18d332990b75
Tiago Justino <tiago.vmj@gmail.com>
2016-08-19 14:27:39 -0300

    week 2 lesson 3 exercise

--- full-stack-web-development/03-angularjs/conFusion-gulp/app/scripts/app.js
+++ full-stack-web-development/03-angularjs/conFusion-gulp/app/scripts/app.js
@@ -1,17 +1,18 @@
-'use strict';
-
-angular.module('confusionApp', []).controller('menuController', function() {
-  this.tab = 0;
-  this.filtText = '';
-  var dishes=[
-    {
-      name:'Uthapizza',
-      image: 'images/uthapizza.png',
-      category: 'mains',
-      label:'Hot',
-      price:'4.99',
-      description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-      comment: '' },
+angular.module('confusionApp', [])
+  .controller('MenuController', ['$scope', function($scope) {
+    'use strict';
+    $scope.showDetails = false;
+    $scope.tab = 0;
+    $scope.filtText = '';
+    $scope.dishes=[
+      {
+        name:'Uthapizza',
+        image: 'images/uthapizza.png',
+        category: 'mains',
+        label:'Hot',
+        price:'4.99',
+        description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+        comment: '' },
       {
         name:'Zucchipakoda',
         image: 'images/zucchipakoda.png',
@@ -20,30 +21,99 @@ angular.module('confusionApp', []).controller('menuController', function() {
         price:'1.99',
         description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
         comment: '' },
-        {
-          name:'Vadonut',
-          image: 'images/vadonut.png',
-          category: 'appetizer',
-          label:'New',
-          price:'1.99',
-          description:'A quintessential ConFusion experience, is it a vada or is it a donut?',
-          comment: '' },
-          {
-            name:'ElaiCheese Cake',
-            image: 'images/elaicheesecake.png',
-            category: 'dessert',
-            label:'',
-            price:'2.99',
-            description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
-            comment: '' }
-  ];
-  this.dishes = dishes;
+      {
+        name:'Vadonut',
+        image: 'images/vadonut.png',
+        category: 'appetizer',
+        label:'New',
+        price:'1.99',
+        description:'A quintessential ConFusion experience, is it a vada or is it a donut?',
+        comment: '' },
+      {
+        name:'ElaiCheese Cake',
+        image: 'images/elaicheesecake.png',
+        category: 'dessert',
+        label:'',
+        price:'2.99',
+        description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
+        comment: '' }
+    ];
 
-  this.select = function(setTab) {
-    this.tab = setTab;
-    this.filtText = ["", "appetizer", "mains", "desser"][this.tab];
-  };
-  this.isSelected = function (checkTab) {
-    return (this.tab === checkTab);
-  };
-});
+    $scope.select = function(setTab) {
+      $scope.tab = setTab;
+      $scope.filtText = ["", "appetizer", "mains", "desser"][$scope.tab];
+    };
+    $scope.isSelected = function (checkTab) {
+      return ($scope.tab === checkTab);
+    };
+    $scope.toggleDetails = function() {
+      $scope.showDetails = !$scope.showDetails;
+    };
+  }])
+  .controller('ContactController', ['$scope', function($scope) {
+    $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+    var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+    $scope.channels = channels;
+    $scope.invalidChannelSelection = false;
+  }])
+  .controller('FeedbackController', ['$scope', function($scope) {
+    $scope.sendFeedback = function() {
+      console.log($scope.feedback);
+      if ($scope.feedback.agree && ($scope.feedback.mychannel == "")&& !$scope.feedback.mychannel) {
+        $scope.invalidChannelSelection = true;
+        console.log('incorrect');
+      }
+      else {
+        $scope.invalidChannelSelection = false;
+        $scope.feedback = {mychannel:"", firstName:"", lastName:"",
+          agree:false, email:"" };
+        $scope.feedback.mychannel="";
+
+        $scope.feedbackForm.$setPristine();
+        console.log($scope.feedback);
+      }
+    };
+  }])
+  .controller('DishDetailController', ['$scope', function($scope) {
+    $scope.dish={
+      name:'Uthapizza',
+      image: 'images/uthapizza.png',
+      category: 'mains', 
+      label:'Hot',
+      price:'4.99',
+      description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+      comments: [
+        {
+          rating:5,
+          comment:"Imagine all the eatables, living in conFusion!",
+          author:"John Lemon",
+          date:"2012-10-16T17:57:28.556094Z"
+        },
+        {
+          rating:4,
+          comment:"Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+          author:"Paul McVites",
+          date:"2014-09-05T17:57:28.556094Z"
+        },
+        {
+          rating:3,
+          comment:"Eat it, just eat it!",
+          author:"Michael Jaikishan",
+          date:"2015-02-13T17:57:28.556094Z"
+        },
+        {
+          rating:4,
+          comment:"Ultimate, Reaching for the stars!",
+          author:"Ringo Starry",
+          date:"2013-12-02T17:57:28.556094Z"
+        },
+        {
+          rating:2,
+          comment:"It's your birthday, we're gonna party!",
+          author:"25 Cent",
+          date:"2011-12-02T17:57:28.556094Z"
+        }
+
+      ]
+    };
+  }]);
