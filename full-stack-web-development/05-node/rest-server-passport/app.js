31089274e4397c8144f62529d6bd64b8f8e5dc75
Tiago Justino <tiago.vmj@gmail.com>
2016-12-29 23:56:59 -0200

    loopback exercise

--- full-stack-web-development/05-node/rest-server-passport/app.js
+++ full-stack-web-development/05-node/rest-server-passport/app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
 
 var config = require('./config');
 
@@ -14,8 +13,8 @@ mongoose.connect(config.mongoUrl);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
-    // we're connected!
-    console.log("Connected correctly to server");
+	// we're connected!
+	console.log("Connected correctly to server");
 });
 
 var routes = require('./routes/index');
@@ -25,6 +24,15 @@ var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
 var app = express();
+
+// Secure traffic only
+app.all('*', function(req, res, next){
+	console.log('req start: ',req.secure, req.hostname, req.url, app.get('port'));
+	if (req.secure) {
+		return next();
+	};
+	res.redirect('https://'+req.hostname+':'+app.get('secPort')+req.url);
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -38,11 +46,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // passport config
-var User = require('./models/user');
 app.use(passport.initialize());
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+var authenticate = require('./authenticate');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -54,32 +59,32 @@ app.use('/leadership',leaderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handlers
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.json({
-      message: err.message,
-      error: err
-    });
-  });
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.json({
+			message: err.message,
+			error: err
+		});
+	});
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: {}
-  });
+	res.status(err.status || 500);
+	res.json({
+		message: err.message,
+		error: {}
+	});
 });
 
 module.exports = app;
--- full-stack-web-development/05-node/rest-server-passport/config.js
+++ full-stack-web-development/05-node/rest-server-passport/config.js
@@ -1,4 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+            clientID: '1839817162901109',
+            clientSecret: '1ec939b309e8621ff231f0fcb1e19b69',
+            callbackURL: 'https://localhost:3443/users/facebook/callback'
+        }
 }
