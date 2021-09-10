e90e7709dbe535f778ec9fbb197ad63a96b4ace5
Tiago Justino <tiago.vmj@gmail.com>
2021-09-09 21:17:08 -0300

    finished assignment

--- algorithms/01-divide-and-conquer-sorting-and-searching-and-randomized-algorithms/qsort/qsort.ts
+++ algorithms/01-divide-and-conquer-sorting-and-searching-and-randomized-algorithms/qsort/qsort.ts
@@ -1,4 +1,5 @@
 import * as _ from 'lodash';
+import * as fs from "fs";
 
 let comparisons: number = 0;
 
@@ -21,8 +22,8 @@ function Partition(A: Array<number>, l: number, r: number, pi: number): number {
     Swap(A, l, pi);
     const p: number = A[l];
     let i: number = l + 1;
-    for(let j: number = l + 1; j <= r; j++) {
-        if(A[j] < p) {
+    for (let j: number = l + 1; j <= r; j++) {
+        if (A[j] < p) {
             Swap(A, i, j);
             i++;
         }
@@ -32,6 +33,7 @@ function Partition(A: Array<number>, l: number, r: number, pi: number): number {
 }
 
 type ChoosePivotFunction = (A: Array<number>, l: number, r: number) => number;
+
 function ChoosePivotLeft(A: Array<number>, l: number, r: number): number {
     return l;
 }
@@ -40,8 +42,32 @@ function ChoosePivotRight(A: Array<number>, l: number, r: number): number {
     return r;
 }
 
+function min2(a: number, b: number): number {
+    return a < b ? a : b;
+}
+
+function min3(a: number, b: number, c: number): number {
+    return min2(min2(a, b), c);
+}
+
+function max2(a: number, b: number): number {
+    return a > b ? a : b;
+}
+
+function max3(a: number, b: number, c: number): number {
+    return max2(max2(a, b), c);
+}
+
+function median3(a: number, b: number, c: number): number {
+    const min = min3(a, b, c);
+    const max = max3(a, b, c);
+    return (a !== min && a !== max) ? a : ((b !== min && b !== max) ? b : c);
+}
+
 function ChoosePivotMedian(A: Array<number>, l: number, r: number): number {
-    return r;
+    const m: number = Math.floor(((r - l) / 2) + l);
+    const median = median3(A[l], A[m], A[r]);
+    return (median === A[l]) ? l : ((median === A[m]) ? m : r);
 }
 
 /* QuickSort (array A, length n)
@@ -52,7 +78,7 @@ function ChoosePivotMedian(A: Array<number>, l: number, r: number): number {
        Recursively sort 2nd part */
 
 function QuickSort(A: Array<number>, l: number, r: number, ChoosePivot: ChoosePivotFunction): void {
-    if(l >= r) {
+    if (l >= r) {
         return;
     }
     comparisons += r - l
@@ -63,32 +89,39 @@ function QuickSort(A: Array<number>, l: number, r: number, ChoosePivot: ChoosePi
     QuickSort(A, npi + 1, r, ChoosePivot);
 }
 
-const x: Array<number> = [1,2,3,4];
-const y: Array<number> = _.shuffle(x);
+// const x: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const y: Array<number> = _.shuffle(x);
+const y: Array<number> = fs.readFileSync('./qsort.txt')
+    .toString()
+    .split('\n')
+    .map(
+        (el: string): number => parseInt(el.trim(), 10)
+    )
+    .filter(
+        (el: number): boolean => !Number.isNaN(el)
+    );
 let z: Array<number>;
 
 console.log('==== LEFT =================');
 z = _.clone(y);
-console.log(`[${z.join(', ')}]`);
+// console.log(`[${z.join(', ')}]`);
 comparisons = 0;
 QuickSort(z, 0, y.length - 1, ChoosePivotLeft);
-console.log(`[${z.join(', ')}]`);
+// console.log(`[${z.join(', ')}]`);
 console.log(`comparisons = ${comparisons}`);
 
 console.log('==== RIGHT ================');
-
 z = _.clone(y);
-console.log(`[${z.join(', ')}]`);
+// console.log(`[${z.join(', ')}]`);
 comparisons = 0;
 QuickSort(z, 0, y.length - 1, ChoosePivotRight);
-console.log(`[${z.join(', ')}]`);
+// console.log(`[${z.join(', ')}]`);
 console.log(`comparisons = ${comparisons}`);
 
 console.log('==== MEDIAN ===============');
-
 z = _.clone(y);
-console.log(`[${z.join(', ')}]`);
+// console.log(`[${z.join(', ')}]`);
 comparisons = 0;
 QuickSort(z, 0, y.length - 1, ChoosePivotMedian);
-console.log(`[${z.join(', ')}]`);
+// console.log(`[${z.join(', ')}]`);
 console.log(`comparisons = ${comparisons}`);
